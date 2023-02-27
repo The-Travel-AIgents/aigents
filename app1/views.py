fb1b2ee80881834678e8d7f7d5185d1cb7254085
Quyum Kehinde <quyumkehinde@gmail.com>
2023-02-28 00:34:02 +0100

    fix: generate guide view

--- app1/views.py
+++ app1/views.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 from app1.services import Guide
+import json
 
 # Create your views here.
 
 
 class App1View(View):
 
-    def get(self, request):
-        response = Guide().generate('Who is the president of the United States?')
-        data = response.get('choices')[0].text
+    def post(self, request):
+
+        body_bytes = request.body.decode('utf-8')
+        body = json.loads(body_bytes)
+        prompt = body.get('prompt')
+
+        if not prompt:
+            return JsonResponse({
+                'message': 'The prompt field is required.'
+            }, status=400)
+
+        try:
+            response = Guide().generate(prompt=prompt)
+            data = response.get('choices')[0].text
+        except:
+            return JsonResponse({
+                'message': 'An internal server error occured.'
+            })
+
         return JsonResponse({
             'message': 'Successfully generated guide',
             'data': data
