a827d1817805d899c8255a337dacdbd725828653
Quyum Kehinde <quyumkehinde@gmail.com>
2023-02-28 07:00:30 +0100

    fix: added static root env to settings

--- aigents/settings.py
+++ aigents/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
