863cc484a62ea127d65444db5bb24e774b881e58
Quyum Kehinde <quyumkehinde@gmail.com>
2023-02-28 07:32:28 +0100

    fix: update DEBUG settings

--- aigents/settings.py
+++ aigents/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "somesecretkey"
+SECRET_KEY = 'somesecretkey'
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('APP_DEBUG') == 'true'
 
 ALLOWED_HOSTS = []
 
