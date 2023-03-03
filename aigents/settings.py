cb2ebbf0e180b6ecdf01c94d24354bc6b85cb47b
Quyum Kehinde <quyumkehinde@gmail.com>
2023-03-03 04:11:14 +0100

    fix: cors

--- aigents/settings.py
+++ aigents/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://travel-aigent.herokuapp.com',
+    'https://travel-aigent-app.herokuapp.com',
 ]
 
 ROOT_URLCONF = 'aigents.urls'
