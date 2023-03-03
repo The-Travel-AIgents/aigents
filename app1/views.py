a655fbafb68f02fb3c78f416deb8510aff6ae1bc
Quyum Kehinde <quyumkehinde@gmail.com>
2023-03-03 10:51:43 +0100

    chore: added sample response to debug

--- app1/views.py
+++ app1/views.py
@@ -23,11 +23,11 @@ class App1View(View):
         hotel_area = body.get('hotel_area')
         destination = body.get('destination')
 
-        if not dates_and_places or not duration or not start_date or not activities or not start_time or not end_time or not hotel_area or not destination:
-            return JsonResponse({
-                'success': False,
-                'message': 'Please fill in all the required fields.'
-            }, status=400)
+        # if not dates_and_places or not duration or not start_date or not activities or not start_time or not end_time or not hotel_area or not destination:
+        return JsonResponse({
+            'success': False,
+            'message': 'Please fill in all the required fields.'
+        }, status=400)
 
         customized_place_query = ''
         activities_query = ''
